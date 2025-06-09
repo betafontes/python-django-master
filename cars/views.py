bed6c9cb337e519813a68d4f13beecd80d0c3d56
Roberta Fontes <70981960+betafontes@users.noreply.github.com>
2025-06-09 16:05:45 -0300

    ✨ fix(core): Reescrevendo new_car_view

--- cars/views.py
+++ cars/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.db.models import Q
 
 class CarsView(View):
     def get(self, request):
@@ -9,21 +10,29 @@ class CarsView(View):
         search = request.GET.get('search')
 
         if search:
-            cars = cars.filter(model__icontains=search)
+            query = Q(model__icontains=search) | Q(brand__name__icontains=search) | Q(plate__icontains=search)
 
-        return render(
-            request,
-            'cars.html',
-            {'cars': cars}
-        )
-    
+            # tentar converter para int/float para aplicar nos campos numéricos
+            if search.isdigit():
+                query |= Q(factory_year=search) | Q(model_year=search)
+            try:
+                value = float(search.replace(',', '.'))
+                query |= Q(value=value)
+            except ValueError:
+                pass
+            cars = cars.filter(query)
 
-def new_car_view(request):
-    if request.method == 'POST':
+        return render(request, 'cars.html', {'cars': cars})
+
+
+class NewCarView(View):
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
